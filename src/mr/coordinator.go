523a0a9e821530b8a63583e6a3dcb8a59cf581fa
zhouzihong <aqua5201314@outlook.com>
2022-05-05 08:26:41 -0700

    finish lab1

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,29 +1,168 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"time"
+)
 
+const (
+	MaxTaskRunInterval = time.Second * 10
+)
 
-type Coordinator struct {
-	// Your definitions here.
-
+type Task struct {
+	fileName  string
+	id        int
+	startTime time.Time
+	status    TaskStatus // 状态：任务是否完成，是否map任务完成，是否reduce任务完成
 }
 
-// Your code here -- RPC handlers for the worker to call.
+/* 
+Coordinator是无状态的，不用保存worker数组
+需要维护Task数组
+*/
+type Coordinator struct {
+	files   []string 
+	nReduce int
+	nMap    int 
+	phase   SchedulePhase // 状态：任务是否完成，是否map任务完成，是否reduce任务完成
+	tasks   []Task
 
-//
-// an example RPC handler.
-//
+	heartbeatCh chan heartbeatMsg
+	reportCh    chan reportMsg
+	doneCh      chan struct{}
+}
+
+type heartbeatMsg struct {
+	response *HeartbeatResponse
+	ok       chan struct{}
+}
+
+type reportMsg struct {
+	request *ReportRequest
+	ok      chan struct{}
+}
+
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) Heartbeat(request *HeartbeatRequest, response *HeartbeatResponse) error {
+	msg := heartbeatMsg{response, make(chan struct{})}
+	c.heartbeatCh <- msg
+	<-msg.ok
 	return nil
 }
 
+func (c *Coordinator) Report(request *ReportRequest, response *ReportResponse) error {
+	msg := reportMsg{request, make(chan struct{})}
+	c.reportCh <- msg
+	<-msg.ok
+	return nil
+}
+
+// coordinator启动时开启后台goroutine运行schedule
+func (c *Coordinator) schedule() {
+	c.initMapPhase()
+	for {
+		select {
+		// 监控heartbeatCh和reportCh并做处理
+		case msg := <-c.heartbeatCh:
+			if c.phase == CompletePhase {
+				msg.response.JobType = CompleteJob
+			} else if c.selectTask(msg.response) {
+				switch c.phase {
+				case MapPhase:
+					log.Printf("Coordinator: %v finished, start %v \n", MapPhase, ReducePhase)
+					c.initReducePhase()
+					c.selectTask(msg.response)
+				case ReducePhase:
+					log.Printf("Coordinator: %v finished, Congratulations \n", ReducePhase)
+					c.initCompletePhase()
+					msg.response.JobType = CompleteJob
+				case CompletePhase:
+					panic(fmt.Sprintf("Coordinator: enter unexpected branch"))
+				}
+			}
+			log.Printf("Coordinator: assigned a task %v to worker \n", msg.response)
+			msg.ok <- struct{}{}
+		case msg := <-c.reportCh:
+			if msg.request.Phase == c.phase {
+				log.Printf("Coordinator: Worker has executed task %v \n", msg.request)
+				c.tasks[msg.request.Id].status = Finished
+			}
+			msg.ok <- struct{}{}
+		}
+	}
+}
+
+func (c *Coordinator) selectTask(response *HeartbeatResponse) bool {
+	allFinished, hasNewJob := true, false
+	for id, task := range c.tasks {
+		switch task.status {
+		case Idle:
+			allFinished, hasNewJob = false, true
+			c.tasks[id].status, c.tasks[id].startTime = Working, time.Now()
+			response.NReduce, response.Id = c.nReduce, id
+			if c.phase == MapPhase {
+				response.JobType, response.FilePath = MapJob, c.files[id]
+			} else {
+				response.JobType, response.NMap = ReduceJob, c.nMap
+			}
+		case Working:
+			allFinished = false
+			if time.Now().Sub(task.startTime) > MaxTaskRunInterval {
+				hasNewJob = true
+				c.tasks[id].startTime = time.Now()
+				response.NReduce, response.Id = c.nReduce, id
+				if c.phase == MapPhase {
+					response.JobType, response.FilePath = MapJob, c.files[id]
+				} else {
+					response.JobType, response.NMap = ReduceJob, c.nMap
+				}
+			}
+		case Finished:
+		}
+		if hasNewJob {
+			break
+		}
+	}
+
+	if !hasNewJob {
+		response.JobType = WaitJob
+	}
+	return allFinished
+}
+
+func (c *Coordinator) initMapPhase() {
+	c.phase = MapPhase
+	c.tasks = make([]Task, len(c.files))
+	for index, file := range c.files {
+		c.tasks[index] = Task{
+			fileName: file,
+			id:       index,
+			status:   Idle,
+		}
+	}
+}
+
+func (c *Coordinator) initReducePhase() {
+	c.phase = ReducePhase
+	c.tasks = make([]Task, c.nReduce)
+	for i := 0; i < c.nReduce; i++ {
+		c.tasks[i] = Task{
+			id:     i,
+			status: Idle,
+		}
+	}
+}
+
+func (c *Coordinator) initCompletePhase() {
+	c.phase = CompletePhase
+	c.doneCh <- struct{}{}
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -32,9 +171,9 @@ func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
 	//l, e := net.Listen("tcp", ":1234")
-	sockname := coordinatorSock()
-	os.Remove(sockname)
-	l, e := net.Listen("unix", sockname)
+	name := coordinatorSock()
+	os.Remove(name)
+	l, e := net.Listen("unix", name)
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
@@ -46,12 +185,8 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	<-c.doneCh
+	return true
 }
 
 //
@@ -60,11 +195,15 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
-
+	c := Coordinator{
+		files:       files,
+		nReduce:     nReduce,
+		nMap:        len(files),
+		heartbeatCh: make(chan heartbeatMsg),
+		reportCh:    make(chan reportMsg),
+		doneCh:      make(chan struct{}, 1),
+	}
 	c.server()
+	go c.schedule()
 	return &c
 }
